3d2527e69fabb4dd20e2773bcda5c749f03531e9
Isaac Lichter <isaacilichter@gmail.com>
2021-05-02 11:41:28 -0400

    Added unit component

---
+++ core/src/main/java/org/strategyGame/units/components/UnitComponent.java
@@ -0,0 +1,11 @@
+package org.strategyGame.units.components;
+
+import org.terasology.gestalt.entitysystem.component.Component;
+
+public class UnitComponent implements Component<UnitComponent> {
+
+    @Override
+    public void copy(UnitComponent other) {
+
+    }
+}
